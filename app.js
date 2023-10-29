64f724eb27d5c1afc8db4d3c84840132c8ab01eb
Oleh Pona <lanuoleg@gmail.com>
2023-10-29 22:48:00 +0200

    jokeHUB 1.0

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -7,7 +8,7 @@ var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
 var app = express();
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/api.js
+++ routes/api.js
@@ -4,27 +4,84 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 router.post("/add" , async function (req,res,next) {
-    await prisma.jokes.create({
-        data: {
-            name : req.body.name,
-            type: req.body.type,
-            author : req.body.author,
-            body : req.body.body
-        }
-    })
-    res.send("OK")
+    try{
+        console.log(req)
+        await prisma.jokes.create({
+            data: {
+                name : req.body.name,
+                type: req.body.type,
+                author : req.body.author,
+                body : req.body.body
+            }
+        })
+        res.send("OK")
+    } catch (e) {
+        res.send("Internal error: " + e.message);
+    }
+
 })
 router.post("/jokes",async function (req, res, next){
-    if(req.body.id === "all"){
-        res.send(await prisma.jokes.findMany())
-    }else{
-        res.send(await prisma.jokes.findMany({
-            where : {
-                id :{
-                   in : req.body.id
-                }
+    try{
+        if(req.body.id === "all"){
+            if (req.body.type === "all"){
+                res.send(await prisma.jokes.findMany())
+            } else {
+                res.send(await prisma.jokes.findMany({
+                    where :{
+                        type : req.body.type
+                    }
+                }))
             }
-        }));
+
+        }else {
+            if (req.body.type === "all") {
+                res.send(await prisma.jokes.findMany({
+                    where: {
+                        id: {
+                            in: req.body.id
+                        }
+                    }
+                }));
+            } else {
+                res.send(await prisma.jokes.findMany({
+                    where: {
+                        id: {
+                            in: req.body.id
+                        },
+                        type: req.body.type
+                    }
+                }));
+            }
+        }
+    } catch (e) {
+        res.send("Internal error: " + e.message);
     }
+})
+router.post("/delete" ,async function (req,res,next) {
+    console.log(req.body.id);
+    try{
+        let prismaRes = await prisma.jokes.delete({
+            where:{
+                id: req.body.id
+            }
+        })
+        res.send("OK");
+    } catch (e){
+        res.send("Internal error: "+e.message)
+    }
+})
+router.post("/update", async function (req,res,next){
+    const prismaRes = await prisma.jokes.update({
+        where : {
+            id:req.body.id
+        },
+        data:{
+            name : req.body.data.name,
+            type: req.body.data.type,
+            author : req.body.data.author,
+            body : req.body.data.body
+        }
+    })
+    res.send("OK");
 })
 module.exports = router;
